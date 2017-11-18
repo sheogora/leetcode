92fcb04bce5abd0cd8134ff6fb1cd42833792ccb
plunderbun <kravceva@tcd.ie>
2017-11-18 15:51:09 +0000

    test

--- findPower.js
+++ findPower.js
@@ -1,5 +1,7 @@
 // Given a positive integer which fits in a 32 bit signed integer, find if it can be expressed as A^P where P > 1 and A > 0. A and P both should be integers.
 
-function findPower() {
+function findPower(value) {
 	
+	return [int, ]
 }
+
